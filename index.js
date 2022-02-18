07340c2c98e8cbc97cc5c92e9cd3df399b051de5
saurabhchirde <saurabh123.chirde@gmail.com>
2022-02-18 14:37:37 +0530

    refactor code, update: signup, wishlist

--- index.js
+++ index.js
@@ -2,32 +2,23 @@ const loginBtn = document.querySelectorAll(".signin");
 const createAccBtn = document.querySelectorAll(".signup");
 const modalCloseBtn = document.querySelectorAll(".close-btn-modal");
 
-
-const showLoginModal = ()=> {
+const showLoginModal = () => {
     document.querySelector(".signin-modal").classList.remove("hide");
     document.querySelector(".signup-modal-one").classList.add("hide");
 }
 
-const showSignupModal = ()=> {
+const showSignupModal = () => {
     document.querySelector(".signup-modal-one").classList.remove("hide");
     document.querySelector(".signin-modal").classList.add("hide");
 }
 
-const closeAllModal = ()=> {
+const closeAllModal = () => {
     document.querySelector(".signup-modal-one").classList.add("hide");
     document.querySelector(".signin-modal").classList.add("hide");
 }
 
-loginBtn.forEach(btn=>btn.addEventListener("click",showLoginModal));
+loginBtn.forEach(btn => btn.addEventListener("click", showLoginModal));
 
-createAccBtn.forEach(btn=>btn.addEventListener("click",showSignupModal));
+createAccBtn.forEach(btn => btn.addEventListener("click", showSignupModal));
 
-modalCloseBtn.forEach(btn=>btn.addEventListener("click",closeAllModal));
-
-document.querySelector(".show-filter-section-btn").addEventListener("click",()=>{
-    document.querySelector(".filter-section").style.display="inline";
-})
-
-document.querySelector(".filter-close-btn").addEventListener("click",()=>{
-    document.querySelector(".filter-section").style.display="none";
-})
+modalCloseBtn.forEach(btn => btn.addEventListener("click", closeAllModal));
---
+++ products.js
@@ -0,0 +1,6 @@
+document.querySelector(".show-filter-section-btn").addEventListener("click", () => {
+    document.querySelector(".filter-section").style.display = "inline";
+});
+document.querySelector(".filter-close-btn").addEventListener("click", () => {
+    document.querySelector(".filter-section").style.display = "none";
+});
